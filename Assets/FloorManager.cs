a140534ebb19dc7c55f87468fd5ff1bfc57f454b
Agustin Pontura <pontura@gmail.com>
2018-06-02 13:20:02 -0300

    cambios varios

--- Assets/FloorManager.cs
+++ Assets/FloorManager.cs
@@ -7,7 +7,8 @@ public class FloorManager : MonoBehaviour {
 	
 	public void Init (CharactersManager charactersManager) {
 
-        if (Data.Instance.isArcadeMultiplayer) return;
+       // if (Data.Instance.isArcadeMultiplayer) return;
+		if (Data.Instance.playMode == Data.PlayModes.VERSUS) return;
 
 	    Floor newFloor = Instantiate(startingFloor) as Floor;
         newFloor.Init(charactersManager);
