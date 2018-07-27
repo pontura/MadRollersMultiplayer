488b2d1c9772a74fac37573083eac0109fd243a4
Agustin Pontura <pontura@gmail.com>
2018-07-26 23:18:57 -0300

    gargola

--- Assets/MMOVolador.cs
+++ Assets/MMOVolador.cs
@@ -7,12 +7,33 @@ public class MMOVolador : MmoCharacter {
 	public float Distance_to_react;
 	public Animation anim;
 	bool isOn;
+	float initial_speed=2.5f;
+	float speed;
+	float acceleration = 10f;
 
+	public override void OnRestart(Vector3 pos)
+	{
+		speed = initial_speed;
+		isOn = false;
+		base.OnRestart (pos);
+		GetComponent<Rigidbody> ().useGravity = true;
+	}
 	public override void OnSceneObjectUpdate()
 	{
-		if (!isOn && distanceFromCharacter < Distance_to_react) {
-			isOn = true;
-			anim.Play ("on");
+		if ( distanceFromCharacter < Distance_to_react) {
+			
+			if(!isOn)
+			{
+				GetComponent<Rigidbody> ().useGravity = false;
+				isOn = true;
+				anim.Play ("gargola_fly");
+			}
+			speed += Time.deltaTime*acceleration;
+
+			Vector3 pos = transform.localPosition;
+			pos += -transform.forward * Time.deltaTime * speed;
+			pos.y +=  Time.deltaTime * speed;
+			transform.localPosition = pos;
 		}
 	}
 }
